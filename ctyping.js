d2a7166f5d2963a9a77ad3e82f91f66a92497b39
come25136 <come25136@gmail.com>
2017-02-15 12:10:52 +0900

    [add] Init commit

---
+++ ctyping.js
@@ -0,0 +1,20 @@
+(function ($) {
+  $.fn.ctyping = function (value, min, max) {
+    return this.each(function () {
+      var $this = $(this);
+      var values = value.split("");
+      var count = 0;
+
+      function add() {
+        $this.append(values[count]);
+        count++;
+
+        if (count !== values.length) {
+          setTimeout(add, Math.floor(Math.random() * (++max - min)) + min);
+        }
+      }
+
+      add();
+    });
+  };
+})(jQuery);
